5eea58fc113eabcd84230e1fed358e339d06016a
Alexander <satan1c@ukr.net>
2022-04-26 15:41:44 +0300

    Allocation free 'ToString' for enums

--- Types/Extensions.cs
+++ Types/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using WorldOfTanksBlitz.Types.Enums;
 
 namespace WorldOfTanksBlitz.Types
@@ -8,7 +9,62 @@ namespace WorldOfTanksBlitz.Types
         [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
         internal static bool HasService(this Service services, Service target)
         {
-            return ((byte) services & (byte) target) != 0;
+            return ((byte)services & (byte)target) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        public static string FormatToString(this Format target)
+        {
+            return target switch
+            {
+                Format.List => nameof(Format.List),
+                Format.Info => nameof(Format.Info),
+                Format.Achievements => nameof(Format.Achievements),
+                Format.TankStats => nameof(Format.TankStats),
+                Format.Stats => nameof(Format.Stats),
+                Format.AccountInfo => nameof(Format.AccountInfo),
+                Format.Teams => nameof(Format.Teams),
+                Format.Stages => nameof(Format.Stages),
+                Format.Matches => nameof(Format.Matches),
+                Format.Standings => nameof(Format.Standings),
+                Format.Tables => nameof(Format.Tables),
+                Format.Vehicles => nameof(Format.Vehicles),
+                Format.VehicleProfile => nameof(Format.VehicleProfile),
+                Format.Modules => nameof(Format.Modules),
+                Format.Provisions => nameof(Format.Provisions),
+                Format.CrewSkills => nameof(Format.CrewSkills),
+                Format.VehicleProfiles => nameof(Format.VehicleProfiles),
+                _ => throw new ArgumentOutOfRangeException(nameof(target), target, null)
+            };
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        public static string FormatToString(this Sections target)
+        {
+            return target switch
+            {
+                Sections.Account => nameof(Sections.Account),
+                Sections.Encyclopedia => nameof(Sections.Encyclopedia),
+                Sections.Clans => nameof(Sections.Clans),
+                Sections.Tanks => nameof(Sections.Tanks),
+                Sections.Tournaments => nameof(Sections.Tournaments),
+                _ => throw new ArgumentOutOfRangeException(nameof(target), target, null)
+            };
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        public static string FormatToString(this Service target)
+        {
+            return target switch
+            {
+                Service.None => nameof(Service.None),
+                Service.Accounts => nameof(Service.Accounts),
+                Service.Encyclopedia => nameof(Service.Encyclopedia),
+                Service.Clans => nameof(Service.Clans),
+                Service.PlayerVehicles => nameof(Service.PlayerVehicles),
+                Service.Tournaments => nameof(Service.Tournaments),
+                _ => throw new ArgumentOutOfRangeException(nameof(target), target, null)
+            };
         }
     }
 }
