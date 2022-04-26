155a60f06e700c1e837f2db41d69dfe0b142bdc0
Alexander <satan1c@ukr.net>
2022-04-26 15:46:41 +0300

    Create TournamentState enum for tournament state

---
+++ Types/Tournaments/Enums/TournamentState.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace WorldOfTanksBlitz.Types.Tournaments.Enums
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TournamentState : byte
+    {
+        None,
+        
+        [EnumMember(Value = "waiting_results")]
+        WaitingResults,
+        [EnumMember(Value = "got_results")]
+        GotResults,
+        
+        Upcoming,
+        Canceled,
+    }
+}
