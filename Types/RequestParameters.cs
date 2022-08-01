ebfcda41bad8c49229c03fc36bffb9a7740604e4
Alexander <satan1c@ukr.net>
2022-08-01 14:02:23 +0300

    RequestArguments fix

--- Types/RequestParameters.cs
+++ Types/RequestParameters.cs
@@ -17,6 +17,8 @@ namespace WorldOfTanksBlitz.Types
         [JsonProperty("tournament_id")] public IEnumerable<long>? TournamentId { get; set; }
 
         [JsonProperty("clan_id")] public IEnumerable<long>? ClanId { get; set; }
+        
+        [JsonProperty("stage_id")] public IEnumerable<long>? StageId { get; set; }
 
         [JsonProperty("team_id")] public IEnumerable<long>? TeamId { get; set; }
 
@@ -26,9 +28,14 @@ namespace WorldOfTanksBlitz.Types
     public class RequestParameters<T> : RequestParameters
     {
         [JsonProperty("tank_id")] public new T TankId { get; set; } = default!;
+        
+        [JsonProperty("stage_id")] public new T StageId { get; set; } = default!;
 
         [JsonProperty("tournament_id")] public new T TournamentId { get; set; } = default!;
-
+    }
+    
+    public class RequestParameters<T, TT> : RequestParameters<T>
+    {
         public new TT Status { get; set; } = default!;
     }
 }
